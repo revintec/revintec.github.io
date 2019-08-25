0774a44ed66b74c6240e1108e5bfd1321125bd70
云斌 <caoyunbin001@126.com>
2019-08-25 16:23:03 +0800

    Add files via upload

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 sudo yum install -y wget zsh net-tools rsync tmux java
-for i in .zshrc rsyncd.conf corex-*.jar(N); do mv -v $i $i.bak; done; wget http://revintec.github.io/corex-{unspecified-main,jvm}.jar
+for i in .zshrc rsyncd.conf corex-*.jar(N); do mv -v $i $i.bak; done; wget -N http://revintec.github.io/corex-{unspecified-main,jvm}.jar
 PATH=$PATH:. java -cp corex-unspecified-main.jar StarBaseKt deploy
