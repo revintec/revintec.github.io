ff8d641b6c20a29a2c9f91e68f6c725c902c3036
云斌 <caoyunbin001@126.com>
2019-09-12 13:53:57 +0800

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-corex=corex-unspecified-main.jar
-sudo yum install -y wget zsh net-tools rsync tmux java
-wget -N http://revintec.github.io/{$corex,corex-jvm.jar}
-PATH=$PATH:. java -cp $corex StarBaseKt deploy
